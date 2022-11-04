995a216c778202598ce8923083949eb5672dd584
dariachernova <dariaiakovleva10u1@gmail.com>
2022-11-05 00:44:19 +0300

    new

---
+++ homework.js
@@ -0,0 +1,100 @@
+let a = 5;
+let b = 4;
+console.log (a>b);
+
+let c = "ананас";
+let d = "яблоко";
+console.log (c>d);
+
+let e = "2"; //сравниает первые символы первой строки 2
+let f ="12"; // 1 первый символ первой строки
+console.log (e>f);// 2 больше 1 верно
+
+let g = undefined;
+let h = null;
+console.log (g==h);//= это правило языка
+
+let i = undefined;//0
+let n = null;//nan
+console.log (i===n);//false
+
+let j = null;//0
+let k = "/n0/n";//
+console.log (j==k);//false
+
+let l = null;
+let m = +"/n0/n";
+console.log (l===m);//false строгое неравенство разные типы
+
+let o = "";
+let p = 1;
+let q = 0;
+console.log (o+p+q);
+
+let r = "";
+let s = 1;
+let t = 0;
+console.log (r-s+t);
+
+let u = true;//1
+let v = false;//0
+console.log (u+v);
+
+let w = 6;
+let x = "3";//3
+console.log (w/x);
+
+let y = "2";
+let z = "3";
+console.log (y*z);
+
+let ab = 4;
+let ac = 5;
+let ad= "px";
+console.log (ab+ac+ad);
+
+let af = "$";
+let ag = 4;
+let ah= 5;
+console.log (af+ag+ah);
+
+let ai = "4";
+let aj = 2;
+console.log (ai-aj);
+
+let ak = "4px";
+let al = 2;
+console.log (ak-al);
+
+let am = 7;
+let an = 0;
+console.log (am/an);
+
+let ao = "-9";
+let ap = 5;
+console.log (ao+ap);
+
+let aq = "-9";
+let ar = 5;
+console.log (aq-ar);
+
+let as = null;
+let at = 1;
+console.log (as+at);
+
+let au = undefined;
+let av = 1;
+console.log (au+av);
+
+let aw = "/t/n";
+let ax = 2;
+console.log (aw-ax);
+
+
+
+
+
+
+
+
+
